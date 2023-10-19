fe4cc9f763e838d15acb89a1212bd1637a65cf47
Samuel <eb.samuel6@gmail.com>
2023-10-19 15:40:20 -0500

    Text color changed and request method select removed

--- src/components/requestMaker/requestTitle/index.js
+++ src/components/requestMaker/requestTitle/index.js
@@ -1,33 +1,7 @@
 import React, { useState } from "react";
 import styles from "./requestTitle.module.css";
-import Select from "react-select";
 
 export default function RequestTitle({ userInput }) {
-  const [selectedOption, setSelectedOption] = useState(null);
-
-  const handleSelectChange = (selectedOption) => {
-    setSelectedOption(selectedOption);
-  }
-
-  const options = [
-    { value: "HTTP",      label: "HTTP" },
-    { value: "GraphQL",   label: "GraphQL" },
-    { value: "gRPC",      label: "gRPC" },
-    { value: "WebSocket", label: "WebSocket" },
-    { value: "Socket.IO", label: "Socket.IO" },
-    { value: "MQTT",      label: "MQTT" }
-  ];
-
-  const customStyles = {
-    option: (provided, state) => ({
-      ...provided,  
-      border: "none",
-    }),
-    dropdownIndicator: (provided) => ({
-      ...provided,
-     
-    }),
-  };
 
   const getOptionLabel = (option) => (
     <div style={{ color: option.color }}>{option.label}</div>
@@ -35,16 +9,10 @@ export default function RequestTitle({ userInput }) {
 
   return (
     <div className={styles['title']}>
-      <label className={styles['label']}>
-        <Select
-            className={styles['select']}
-            options={options}
-            value={selectedOption}
-            onChange={handleSelectChange}
-            styles={customStyles}
-            getOptionLabel={getOptionLabel}
-            placeholder={options[0].label}
-          />
+      <label className={styles['label']}> 
+        <span className="pe-2">
+          <img alt="http" src="img/http.png" className={styles['http-icon']}></img>
+        </span>
         <div className={styles['titulo']}>
          {userInput ? userInput : "Untitled Request"}
         </div>
